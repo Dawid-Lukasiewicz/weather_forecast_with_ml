5b5037788c454b1ca45b405c784d503bb2846aaf
Dawid Łukasiewicz <lukasiewicz@onet.com.pl>
2023-11-05 18:20:27 +0100

    remove last row and compare predictions to actual values

--- src/learn.py
+++ src/learn.py
@@ -1,5 +1,6 @@
 # import ridge regression model
 from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_absolute_error
 # packages to handle data
 import pandas as pd
 from pathlib import Path, PurePath
@@ -14,4 +15,18 @@ weather_data = pd.read_csv(data_file_path, index_col="DATE")
 
 # set Ridge's parameter
 model = Ridge(alpha=0.2)
+# list of columns as inputs
+predictors = ["PRECIPITATION", "TMAX", "TMIN"]
+# set range for training
+train = weather_data.loc[:"2021-12-31"]
+# set range for tests
+test = weather_data.loc["2022-01-01":]
 
+# fit the training data for max temperature
+model.fit(train[predictors], train["TARGET_TMAX"])
+
+# get the prediction from model
+predictions = model.predict(test[predictors])
+# compare predictions to actual values and get the error value
+error = mean_absolute_error(test["TARGET_TMAX"], predictions)
+print("Mean absolute error between actual and predictions: ", error)
